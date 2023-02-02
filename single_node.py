ff634dc6efa974083fcbba7348a1480cc4d254a6
Paul Gavrikov <paul.gavrikov@hs-offenburg.de>
2023-02-02 15:37:37 +0000

    adding option to shuffle commands + better logs

--- single_node.py
+++ single_node.py
@@ -9,12 +9,13 @@ def gpu_worker(i, gpu, q):
     logger = logging.getLogger(f"Worker-{i}@cuda:{gpu}")
     while not q.empty():
         cmd = q.get().replace("%gpu%", str(gpu))
-        logger.info(f"Executing {cmd} on GPU {gpu}")
+        logger.info(f"{cmd}")
         status = os.system(cmd)
         if status < 0:
-            logger.error(f"{cmd} FAILED with status {status}")
+            logger.error(f"FAILED (status: {status})")
         else:
-            logger.info(f"{cmd} finished with status {status}")
+            logger.info(f"OKAY (status: {status})")
+    logger.warning("shutting down - queue empty")
 
 
 if __name__ == "__main__":
@@ -24,18 +25,25 @@ if __name__ == "__main__":
     parser.add_argument("command_file", type=str, help="File containing commands to run.")
     parser.add_argument("--gpus", type=str, default="0,1,2,3", help="Comma separated list of GPUs to use.")
     parser.add_argument("--workers_per_gpu", type=int, default=1, help="Number of workers per GPU.")
+    parser.add_argument("--shuffle", action="store_true", default=False, help="Shuffle commands.")
 
     args = parser.parse_args()
 
     que = mp.Queue()
-    commands = 0
+    num_commands = 0
+    commands = []
 
     with open(args.command_file, "r") as f:
         for line in f.readlines():
-            que.put(line.strip())
-            commands += 1
+            commands.append(line.strip())
+            num_commands += 1
+            
+    if args.shuffle:
+        import random
+        random.shuffle(commands)
 
-    logging.info(f"Queued {commands} commands from {args.command_file}")
+    list(map(que.put, commands))
+    logging.info(f"Queued {num_commands} commands from {args.command_file}")
 
     processes = []
     for i, gpu in enumerate(args.gpus.split(",") * args.workers_per_gpu):
